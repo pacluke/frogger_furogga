57ebe0db8c10338f0ef79cade1285a28501e0fb0
Eduardo Stein Brito <eduardobrito14@gmail.com>
2018-04-14 20:07:46 +0000

    Adds basic frog movment

--- testes.jl
+++ testes.jl
@@ -1,3 +1,21 @@
+
+# == move_frog ===============================
+# recebe matriz que representa o mapa e a direção como string
+# limpar a tela
+
+function move_frog(m, left, right, up, down) 
+	position = find(x -> x == "W", m) # Encontra posicao do sapo na matriz
+	m[position] = "." # 'Remove' sapo daquela posição (TODO: deveria ser subsituido por o caracter de acordo com o devido 'terreno' que o sapo se encontra)
+	y,x = ind2sub(m,position) # Converte posicao de indice em posicao representação por tupla
+	new_x_pos = x + right - left
+	new_y_pos = y + down - up
+	# check_for_colisions() Chama metodo que verifica colisoes e etc...
+	m[new_y_pos, new_x_pos] = "W"
+	print_map(m)
+	return m
+end
+# ========================================
+
 # == clear ===============================
 # chama "clear" no terminal para
 # limpar a tela
@@ -5,7 +23,24 @@
 function clear()
 	run(`clear`) # run(x) chama o comando x direto no terminal
 end
+
+# == print_map ===============================
+# Recebe matriz que representa o mapa do jogo
+# Printa essa matriz
+function print_map(map)
+	clear()
+	for j = 1:size(map,1)
+		for i = 1:size(map,2)
+			print(map[j,i])
+		end
+		print("\n\r") # quando stty raw ta ativado, temos que usar \n e \r 
+		# para ir para uma nova linha (\n\r = CRLF)
+	end
+end
 # ========================================
+
+m = readdlm("map.txt")
+print_map(m)
 
 while true
 
@@ -30,30 +65,22 @@ while true
 
 	# == update ==============================
 	# essa rotina serve para atualizar tudo que acontece no jogo
-	m = readdlm("map.txt")
-	for j = 1:size(m,1)
-		for i = 1:size(m,2)
-			print(m[j,i])
-		end
-		print("\n\r")
-	end
 	while(user_input == '+')
-
+		
 		print("")	# precisa existir uma funcao que "faz algo"
 					# para que o @async funcione
 
 		if(user_input == 'a')
-			print("LEFT \n\r")	# quando stty raw ta ativado, temos que usar \n e \r 
-								# para ir para uma nova linha (\n\r = CRLF)
+			m = move_frog(m, 1, 0, 0, 0)
 
 		elseif(user_input == 's')
-			print("DOWN \n\r")
+			m = move_frog(m, 0, 0, 0, 1)
 
 		elseif(user_input == 'd')
-			print("RIGHT \n\r")
+			m = move_frog(m, 0, 1, 0, 0)
 
 		elseif(user_input == 'w')
-			print("UP \n\r")
+			m = move_frog(m, 0, 0, 1, 0)
 
 		elseif(user_input == 'q')
 			print("QUIT GAME \n\r")
